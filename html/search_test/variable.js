ccbbffee9b291659bcbd4db6e44c882acccea60b
gitjae <dlrbwo2022@gmail.com>
2023-03-28 09:30:44 +0900

    testing code

---
+++ html/search_test/variable.js
@@ -0,0 +1,6 @@
+let r1 = {name:"피자집", loc:"강남", score:"4.1"}
+let r2 = {name:"치킨집", loc:"강남", score:"4.0"}
+let r3 = {name:"햄버거집", loc:"역삼", score:"3.5"}
+let r4 = {name:"중국집", loc:"강남", score:"4.5"}
+let r5 = {name:"돈까스집", loc:"역삼", score:"4.2"}
+let r6 = {name:"라면집", loc:"종로", score:"3.9"}
