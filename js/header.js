4547829532df5453dfeeaac004e70ac681ef249c
gitjae <dlrbwo2022@gmail.com>
2023-04-06 17:25:38 +0900

    파비콘

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     let title = $(document).find("title").text()    // 인덱스 페이지만 경로가 ./로 시작
     let startDir = "../"
-    if (title === "index") {
+    if (title === "index" || title === "맛나리") {
         startDir = "./"
     }
     else {
